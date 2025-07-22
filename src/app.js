2d8425e64ed6ac129ac19c5fb85f3542492ab29f
Gaikwad Radhika <radhika.sai.gaikwad@gmail.com>
2025-07-22 13:50:02 +0530

    added

--- src/app.js
+++ src/app.js
@@ -2,14 +2,26 @@ const express =  require('express');
 const port = 3000
 const app = express();
 
-app.use("/hello", (req, res)=>{
-  res.send("Hello from the hello!")
-});
+// this will only handle get cal to /user
 
-app.use("/", (req, res)=>{
-  res.send("Hello from the server!")
-});
+// order matters
+app.use("/user", (req, res)=>{
+  res.send("HAAAAAAAAAAAAAA")
+})
+app.get("/user", (req, res)=>{
+  res.send({name:"Radha", age:23})
+})
 
+app.post("/user", (req, res)=>{
+  // saving data to the DB
+  res.send("Data is saved")
+})
+
+app.delete("/user", (req, res)=>{
+  res.send("deleted!")
+})
+
+// this will match all http method API call to /test
 app.use("/test", (req, res)=>{
   res.send("Hello from the test!")
 });
