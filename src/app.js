4a12b7bd86711846a9c3493e7f7f35e69883cef9
Gaikwad Radhika <radhika.sai.gaikwad@gmail.com>
2025-07-23 12:02:39 +0530

    learned middleware & requst or response handler

--- src/app.js
+++ src/app.js
@@ -2,29 +2,22 @@ const express =  require('express');
 const port = 3000
 const app = express();
 
-// multiple route hanlders
+// Get /users => middlerware chain => request handler 
 
-app.use("/user", [(req, res, next)=>{
-  // route handler
-  console.log("handleing route of the handler function")
-    next();
-  // res.send("Router Handler 1")
-  // by default it not going to the second router hanlder function we need next function
+app.use("/", (req, res, next)=>{
+  // res.send("handling / route")
+  console.log(" hnading /user route & I m just middleware");
+  next();
+});
 
-
-}, (req, res, next)=>{
-
-  console.log("handling second route of the hanlder function");
-next()
-  // res.send("2nd router hanlder!")
+app.get("/user", (req, res, next)=>{
+  console.log(" hnading /user route & I m just middleware");
+  next();
 },
- (req, res, next)=>{
-
-  console.log("handling second route of the hanlder function");
-
-  res.send("5nd router hanlder!")
-}]);
-
+(req, res, next)=>{
+  console.log(" hnading /user route2");
+  res.send("I am request(response) handler because I am sending response back to the client")
+})
 
 
 
