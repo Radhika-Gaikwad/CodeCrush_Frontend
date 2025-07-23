803472dc1d6d763b1c9d19134e4e050c80228276
Gaikwad Radhika <radhika.sai.gaikwad@gmail.com>
2025-07-23 16:03:08 +0530

    get api is created

--- src/app.js
+++ src/app.js
@@ -19,6 +19,66 @@ app.post("/signup", async(req, res, next)=>{
  {
  res.status(400).send("Error is occuring"+ err.message) }
 
+});
+
+// Feed APi get all the users api from the const first = useRef(second)
+
+app.get("/user",async (req, res)=>{
+  const userEmail = req.body.emailId;
+  try{
+    const user = await  User.find({emailId : userEmail});
+    if(user.length===0)
+    {
+      res.status(404).send("User not found")
+      }
+      else{
+      res.send(user)
+      }
+    
+
+  }catch(err)
+  {
+    res.status(400).send("something went wrong")
+  }
+ 
+})
+
+app.get("/feed", async(req, res)=>{
+
+  try{
+    const user = await User.find({});
+    if(user.length===0)
+    {
+      res.status(400).send("No users found")
+   
+    }else{
+         res.send(user)
+    }
+
+  }catch(err)
+  {
+    res.status(400).send("Something went wrong")
+  }
+})
+
+// findOne  
+app.get("/findOne", async(req, res, next)=>{
+const userEmail = req.body.emailId
+  try{
+    const user = await User.findOne({emailId: userEmail});
+console.log(user)
+    if(!user)
+    {
+      res.status(404).send("User is not found")
+    }
+    else{
+      res.send(user)
+    }
+  }catch(err)
+  {
+    res.status(400).end("Something went wrong")
+  }
+  
 })
 
 connectDB()
